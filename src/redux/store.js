40d51e40adf2670dabff59a1542dc2cbab2c1ccc
Athena Kostro <home@Homes-MacBook-Pro.local>
2023-04-17 18:18:35 -0400

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,20 @@
+
+ 
+
+// import { configureStore  } from "reduxjs/toolkit";
+// import { configureStore } from 'redux/toolkit'
+// import rootReducers from "./reducer";
+
+// const store = configureStore({ reducer: rootReducers })
+
+// export default store;
+
+
+
+import { configureStore } from '@reduxjs/toolkit'
+import handleCart from './reducer/handleCart'
+export default configureStore({
+  reducer: {
+    handleCart
+  },
+})
